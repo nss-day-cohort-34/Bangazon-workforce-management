d208c4ff9d5b3fbcb731b7b29d247458cc639a5a
unknown <mstiles01@icloud.com>
2019-11-18 14:31:29 -0600

    Create and Id fixed

---
+++ BangazonWorkforceManagement/SQLQuery10.sql
@@ -0,0 +1,4 @@
+﻿	SELECT d.Name, Count(*)  employee_count, d.Budget
+                    FROM Department d
+                    LEFT JOIN Employee e ON e.DepartmentId = d.Id
+                    GROUP BY d.Name, d.Budget
