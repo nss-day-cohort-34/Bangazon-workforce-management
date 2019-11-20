1ffb83351ff5ed7d1be48f4a2624ddaa20d63008
Bennett Foster <bennfos@gmail.com>
2019-11-20 15:27:50 -0600

    added report to home controller. rendering chart, but no data.

---
+++ BangazonWorkforceManagement/BangazonWorkforceManagement/Models/ViewModels/EmployeeTrainingProgramsReportViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonWorkforceManagement.Models.ViewModels
+{
+    public class EmployeeTrainingProgramsReportViewModel
+    {
+        public List<Department> Departments { get; set; }
+        public List<SelectListItem> DepartmentOptions
+        {
+            get
+            {
+                List<SelectListItem> options = new List<SelectListItem>()
+                {
+                    new SelectListItem("Select a department...", "0")
+                };
+
+                if (Departments != null)
+                {
+                    options.AddRange(
+                        Departments.Select(d => new SelectListItem(d.Name, d.Id.ToString()))
+                    );
+                }
+
+                return options;
+            }
+        }
+    }
+}
